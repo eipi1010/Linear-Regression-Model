e9d2640632479f2ccb5b5396021df96f38c4a03f
Tze Tat Yao <yaot@Tzes-Air.lan>
2025-12-24 11:44:20 +0800

    LinearRegression now works with multiple continuous features, matrix multiplication added, LinearRegression can only calculate mean_square_error of data. Will do gradient descent next

--- src/linear_regression_class.py
+++ src/linear_regression_class.py
@@ -1,31 +1,19 @@
 from imports import *
 
-class linear_regression:
-    def __init__(self, X: pd.Series, y: pd.Series):
-        self.X = X
-        self.y = y
+class LinearRegression:
+    def __init__(self, X: pd.DataFrame, y: pd.Series):
+        self.X = np.asarray(X).reshape(len(X), len(X.axes[1]))
+        self.y = np.asarray(y).reshape(len(y), 1)
     def optimal_weights(self) -> pd.Series:
         pass
 
-    def mean_square_error(self, y_pred: pd.Series) -> float:
-        square_error_sum: float = 0
-        for i in range(len(y_pred)):
-            square_error_sum += (self.y.iloc[i] - y_pred.iloc[i]) ** 2
-        square_error_mean: float = square_error_sum/len(y_pred)
+    def mean_square_error(self, y_pred: np.array) -> float:
+        square_error_mean_short = np.mean((self.y - y_pred) ** 2)
+        return square_error_mean_short
 
-        #square_error_mean = ((self.y - y_pred) ** 2).mean()
-
-        return square_error_mean
-
-    def predict(self) -> pd.Series:
-        y_pred: list[int] = []
-        for x in self.X:
-            y_pred_i = 3*x + 5
-            y_pred.append(y_pred_i)
-
-        y_pred = pd.DataFrame(y_pred)
-        y_pred.columns = ['diagnosed_diabetes']
-        
+    def predict(self, w:np.array, b:float) -> np.array:
+        w = w.reshape((self.X.shape[1], 1))
+        y_pred: np.array = self.X @ w + b
         return y_pred
 
         
